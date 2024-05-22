ae1465bcdca99f827aca5b3754a8d3e030cf72d5
sbx_user1051 <sbx_user1051@169.254.83.93>
2024-05-22 15:07:47 +0000

    Wed May 22 15:07:47 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,12 +3,14 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    msg = ''
-    # validate parameter is a string
-    if isinstance(friend_name, str):
-        msg = f"Hello, {friend_name}"
-    else:
-        msg = "Hello, World!"
-    return msg
+    # msg = ''
+    # # validate parameter is a string
+    # if isinstance(friend_name, str):
+    #     msg = f"Hello, {friend_name}"
+    # else:
+    #     msg = "Hello, World!"
+    # return msg
+    return "Hello, World!"
+
 
 
