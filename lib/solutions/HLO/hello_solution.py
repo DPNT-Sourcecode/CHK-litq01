7c44d9bcfbee0378460c43a11cac133cdc1bfae8
sbx_user1051 <sbx_user1051@169.254.83.93>
2024-05-22 14:57:52 +0000

    Wed May 22 14:57:52 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,5 +4,9 @@
 # friend_name = unicode string
 def hello(friend_name):
     # validate parameter is a string
-    if
+    if isinstance(friend_name, str):
+        return f"Hello, {friend_name}"
+    else:
+        raise ValueError("parameter must be a string")
+
 
